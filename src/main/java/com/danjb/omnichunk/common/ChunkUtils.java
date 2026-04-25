c480b07383a220815779cc6adfcb69b6800b5cf0
danielok2215-debug <danielok2215@gmail.com>
2026-04-24 19:35:11 -0600

    Create ChunkUtils.java

---
+++ src/main/java/com/danjb/omnichunk/common/ChunkUtils.java
@@ -0,0 +1,8 @@
+package com.danjb.omnichunk.common;
+
+public final class ChunkUtils {
+    public static final int CHUNK_SIZE = 16;
+    public static final int CHUNK_HEIGHT = 256;
+
+    private ChunkUtils() {}
+}
