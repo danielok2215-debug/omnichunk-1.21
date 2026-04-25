f05d58da1db87532b53f9c100b55259359877302
danielok2215-debug <danielok2215@gmail.com>
2026-04-24 19:35:54 -0600

    Create ChunkTracker.java

---
+++ src/main/java/com/danjb/omnichunk/server/ChunkTracker.java
@@ -0,0 +1,17 @@
+package com.danjb.omnichunk.server;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.level.ChunkWatchEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ChunkTracker {
+    private static final Logger LOGGER = LogManager.getLogger();
