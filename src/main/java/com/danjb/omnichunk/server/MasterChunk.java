4a7c0fd40bb2c279ff56ffcfaccf5fb2f936bd51
danielok2215-debug <danielok2215@gmail.com>
2026-04-25 13:50:00 -0600

    Update MasterChunk.java

--- src/main/java/com/danjb/omnichunk/server/MasterChunk.java
+++ src/main/java/com/danjb/omnichunk/server/MasterChunk.java
@@ -8,6 +8,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
