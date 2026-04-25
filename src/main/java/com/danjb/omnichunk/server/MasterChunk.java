3ed73a1faac136fcf25bb583cbd2b15dd13972d3
danielok2215-debug <danielok2215@gmail.com>
2026-04-25 13:49:31 -0600

    Update MasterChunk.java

--- src/main/java/com/danjb/omnichunk/server/MasterChunk.java
+++ src/main/java/com/danjb/omnichunk/server/MasterChunk.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,8 +16,5 @@ import java.util.Map;
 
 public class MasterChunk {
     private static final Logger LOGGER = LogManager.getLogger();
-    private static final String NBT_KEY_HEIGHT = "height";
-    private static final String NBT_KEY_TASKS = "tasks";
-    private static final String NBT_KEY_TASK_ID = "taskId";
 
     private static final Map
